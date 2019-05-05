3ca1c4f5b91998727b33bfdf4102a5faf222f621
Max <max.bergmark@gmail.com>
2019-05-05 14:36:46 +0200

    Included wall time

--- mandel_openmp.cpp
+++ mandel_openmp.cpp
@@ -6,6 +6,7 @@
 #include <math.h>
 #include <ctime>
 #include <cstdlib>
+#include <sys/time.h>
 
 #define max(a,b) \
 	({ __typeof__ (a) _a = (a); \
@@ -17,18 +18,21 @@
 		 __typeof__ (b) _b = (b); \
 		_a < _b ? _a : _b; })
 
-// #define XRES 78
-// #define YRES 64
-// #define XRES 310
-// #define YRES 240
-// #define XRES 4096
-// #define YRES 4096
 #define MAX_ITER 99
 
 #define XMIN -3.0
 #define XMAX  2.0
 #define YMIN -1.5
 #define YMAX  1.5
+
+double get_wall_time(){
+	struct timeval time;
+	if (gettimeofday(&time,NULL)){
+		//  Handle error
+		return 0;
+	}
+	return (double)time.tv_sec + (double)time.tv_usec * .000001;
+}
 
 int get_count(std::complex<double> c) {
 	std::complex<double> z = 0 + 0i;
@@ -45,13 +49,13 @@ int main(int argc, char **argv) {
 	int XRES = atoi(argv[1]);
 	int YRES = XRES;
 
-	clock_t t0 = clock();
+	clock_t t0 = clock(); double w0 = get_wall_time();
 
 	MPI_Init(NULL, NULL);
 	int world_rank, world_size;
 	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-	clock_t t1 = clock();
+	clock_t t1 = clock(); double w1 = get_wall_time();
 
 
 	// printf("starting %d/%d\n", world_rank, world_size);
@@ -61,7 +65,7 @@ int main(int argc, char **argv) {
 	int *counts = new int[size*XRES];
 	// int *counts = malloc((size * XRES)*sizeof(int));
 	int *all_counts;
-	clock_t t2 = clock();
+	clock_t t2 = clock(); double w2 = get_wall_time();
 
 	#pragma omp parallel for schedule(dynamic,64)
 	for (int y0 = 0; y0 < size; y0++) {
@@ -75,13 +79,13 @@ int main(int argc, char **argv) {
 		}
 	}
 
-	clock_t t3 = clock();
+	clock_t t3 = clock(); double w3 = get_wall_time();
 
 	if (world_rank == 0) {
 		// all_counts = malloc(XRES * YRES * sizeof(int));
 		all_counts = new int[XRES * YRES];
 	}
-	clock_t t4 = clock();
+	clock_t t4 = clock(); double w4 = get_wall_time();
 
 
 	printf("At end: %d, %d\n", world_rank, size);
@@ -89,7 +93,7 @@ int main(int argc, char **argv) {
 	MPI_Gather(counts, size * XRES, MPI_INT, all_counts, size * XRES, MPI_INT, 0, MPI_COMM_WORLD); 
 	MPI_Finalize();
 
-	clock_t t5 = clock();
+	clock_t t5 = clock(); double w5 = get_wall_time();
 	int sum = 0;
 	if (world_rank == 0) {
 		for (int i = 0; i < YRES; i++) {
@@ -102,12 +106,11 @@ int main(int argc, char **argv) {
 		}
 		printf("sum: %d\n", sum);
 	}
-	clock_t t6 = clock();
-	printf("1: %.5f\n", double(t1 - t0) / CLOCKS_PER_SEC);
-	printf("2: %.5f\n", double(t2 - t1) / CLOCKS_PER_SEC);
-	printf("3: %.5f\n", double(t3 - t2) / CLOCKS_PER_SEC);
-	printf("4: %.5f\n", double(t4 - t3) / CLOCKS_PER_SEC);
-	printf("5: %.5f\n", double(t5 - t4) / CLOCKS_PER_SEC);
-	printf("6: %.5f\n", double(t6 - t5) / CLOCKS_PER_SEC);
-
+	clock_t t6 = clock(); double w6 = get_wall_time();
+	printf("1: %8.5f, %8.5f\n", double(t1 - t0) / CLOCKS_PER_SEC, w1 - w0);
+	printf("2: %8.5f, %8.5f\n", double(t2 - t1) / CLOCKS_PER_SEC, w2 - w1);
+	printf("3: %8.5f, %8.5f\n", double(t3 - t2) / CLOCKS_PER_SEC, w3 - w2);
+	printf("4: %8.5f, %8.5f\n", double(t4 - t3) / CLOCKS_PER_SEC, w4 - w3);
+	printf("5: %8.5f, %8.5f\n", double(t5 - t4) / CLOCKS_PER_SEC, w5 - w4);
+	printf("6: %8.5f, %8.5f\n", double(t6 - t5) / CLOCKS_PER_SEC, w6 - w5);
 }
